de581167a61a3aa0dd5add9d9cbf347efd3fe519
ferchocote <netcote1@hotmail.com>
2017-05-31 23:56:48 -0500

    cambios 2

--- js/Convertidor.js
+++ js/Convertidor.js
@@ -1,10 +1,33 @@
 $(function () {
 	
-	alert('hola mundo');
 	
 });
 
 
 function ConvertirTasa(e){
-	alert($(e).val());
+
+	var selecPeriodo = $("#select-periodo").val();
+    var valorConvertir = parseFloat($("#valor-convertir").val());
+
+	if(selecPeriodo == 0){
+		alert('seleccione un periodo');
+	}
+	
+	if(valorConvertir ==''){
+		alert('Ingrese un Valor a Convertir');
+	}else{
+
+		valorConvertir = (valorConvertir/100);
+	}
+
+    var base = (1 + valorConvertir);
+
+    var exponente = (360/30);
+
+	var ressultado = Math.pow(base, exponente);
+
+	ressultado = (ressultado - 1) * 100;
+
+	alert(parseInt(ressultado));
+
 }
