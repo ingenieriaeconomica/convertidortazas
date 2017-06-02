f6927ebd23756f4ccf52c41dda1fe060b38445ff
williamcon98 <ingeniero4.nuvoll.com>
2017-06-02 00:51:56 -0500

    cambios estilos y tabla datos

--- index.php
+++ index.php
@@ -34,75 +34,77 @@
     </header>
 
     
-    <section class="panel-principal">
-    <div class="row">
+    <div class="container">
+      <div class="row">
+        
+        <div class="col-md-12">
+          <label>Valor de la taza a convertir</label><br>
+          <input type="text" name="valortasaconvertir" id="valor-convertir" required clas="form-control">
+        
+
+         <br> <br> 
+         </div>
+         <div class="col-md-3">
+           <fieldset class="form-group">
+
+              <label>Seleccione el tipo de tasa</label>
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="nominal" checked>Nominal
+                </label>
+              </div>
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="anual" checked>Efectivo
+                </label>
+              </div>
+              <div class="form-check">
+                <label class="form-check-label">
+                  <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>Periodico
+                </label>
+              </div>
       
-      <div class="col-md-12">
-        <label>Valor de la taza a convertir</label><br>
-        <input type="text" name="valortasaconvertir" id="valor-convertir" required clas="form-control">
-      </div>
+          </fieldset>
+          <div class="form-group">
+            <label for="exampleSelect1">Seleccione el tipo de periodo que la tasa remunera</label>
+            <select class="form-control" id="select-periodo">
+              <option value="0">SELECCIONE</option>
+              <option value="1">Días</option>
+              <option value="2">Meses</option>
+              <option value="3">Bimestres</option>
+              <option value="4">Trimestres</option>
+              <option value="5">Semestres</option>
+              <option value="6">Años</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="exampleSelect1">Seleccione la forma de pago de interes</label>
+            <select class="form-control" id="select-forma-pago" >
+              <option value="0">SELECCIONE</option>
+              <option value="1">Anticipado</option>
+              <option value="2">Vencido</option>
+            </select>
+          </div>
 
 
-      <div class="col-md-6">
-         <fieldset class="form-group">
-            <legend>Seleccione el tipo de tasa</legend>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="nominal" checked>Nominal
-              </label>
-            </div>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="anual" checked>Efectivo
-              </label>
-            </div>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="radio" class="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1" checked>Periodico
-              </label>
-            </div>
-    
-        </fieldset>
-        <div class="form-group">
-          <label for="exampleSelect1">Seleccione el tipo de periodo que la tasa remunera</label>
-          <select class="form-control" id="select-periodo">
-            <option value="0">SELECCIONE</option>
-            <option value="1">Días</option>
-            <option value="2">Meses</option>
-            <option value="3">Bimestres</option>
-            <option value="4">Trimestres</option>
-            <option value="5">Semestres</option>
-            <option value="6">Años</option>
-          </select>
+   
+          <!-- Indica una acción exitosa o positiva -->
+          <button type="button" class="btn btn-success" onclick="ValidarTasa()" >Calcular</button>
+
+          <!-- Muestra el botón de forma destacada para descubrir fácilmente
+               el botón principal dentro de un grupo de botones -->
+          <button type="button" class="btn btn-primary" onclick="LimpiarCampos()" >Limpiar</button>
         </div>
 
-        <div class="form-group">
-          <label for="exampleSelect1">Seleccione la forma de pago de interes</label>
-          <select class="form-control" id="select-forma-pago" >
-            <option value="0">SELECCIONE</option>
-            <option value="1">Anticipado</option>
-            <option value="2">Vencido</option>
-          </select>
+          <div class="col-md-9">
+            
+        <?php include_once('vistas/convertidor/tabla.php'); ?>
+
+
         </div>
-
-
- 
-<!-- Indica una acción exitosa o positiva -->
-<button type="button" class="btn btn-success" onclick="ValidarTasa()" >Calcular</button>
-
-<!-- Muestra el botón de forma destacada para descubrir fácilmente
-     el botón principal dentro de un grupo de botones -->
-<button type="button" class="btn btn-primary" onclick="LimpiarCampos()" >Limpiar</button>
-      </div>
-
-        <div class="col-md-6">
-          
-
-
-
       </div>
     </div>
-    </section>
 
     <footer>
 
@@ -111,6 +113,7 @@
     <!-- Le javascript
     ================================================== -->
     <script src="./js/Convertidor.js"></script>
+    <script src="./js/convertidor-v1.js"></script>
 
   </body>
 </html>
---
+++ vistas/convertidor/tabla.php
@@ -0,0 +1,149 @@
+<div class="row">
+	<div class="col-md-12">
+
+	<table class="table table-bordered">
+	    <thead>
+	      <tr>
+
+	      
+	      </tr>
+	    </thead>
+	    <tbody>
+	      <tr>
+			<td><label>Efectivo Anual</label></td>
+	      	<td><input type="text" name="resultadoefectivoanual" id="resultadoefectivoanual" required clas="form-control"> </td>
+	      </tr>
+	      </tbody>
+	</table>
+		
+		        
+	</div>
+	
+	<div class="col-md-6">
+		
+	        <h3>Tasas Nominales</h2>           
+	  <table class="table table-bordered">
+	    <thead>
+	      <tr>
+	      
+	      </tr>
+	    </thead>
+	    <tbody>
+	      <tr>
+	        <td><label>Nominal Anual Diario</label></td>
+	        <td><input type="text" name="nominal-anual-diario" id="nominal-anual-diario" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Nominal Anual Mes Vencido</label></td>
+	        <td><input type="text" name="nominal-anual-mes-vencido" id="nominal-anual-mes-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Nominal Anual Mes Aticipado</label></td>
+	        <td><input type="text" name="nominal-anual-mes-anticipado" id="nnominal-anual-mes-anticipado" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Nominal Anual Bimestre Vencido</label></td>
+	        <td><input type="text" name="nominal-anual-bimestre-vencido" id="nominal-anual-bimestre-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Nominal Anual Bimestre Anticipado</label></td>
+	        <td><input type="text" name="nominal-anual-bimestre-anticipado" id="nominal-anual-bimestre-anticipado" required class="form-control"></td>
+	      </tr>
+
+	      <tr>
+	        <td><label>Nominal Anual Trimestre Vencido</label></td>
+	        <td><input type="text" name="nominal-anual-trimestre-vencido" id="nominal-anual-trimestre-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Nominal Anual Trimestre Anticipado</label></td>
+	        <td><input type="text" name="nominal-anual-trimestre-anticipado" id="nominal-anual-trimestre-anticipado" required class="form-control"></td>
+	      </tr>
+
+	      <tr>
+	        <td><label>Nominal Anual Semestre Vencido</label></td>
+	        <td><input type="text" name="nominal-anual-semestre-vencido" id="nominal-anual-semestre-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Nominal Anual Semestre Anticipado</label></td>
+	        <td><input type="text" name="nominal-anual-semestre-anticipado" id="nominal-anual-semestre-anticipado" required class="form-control"></td>
+	      </tr>
+
+	      <tr>
+	        <td><label>Nominal Anual Año Vencido</label></td>
+	        <td><input type="text" name="nominal-anual-ano-vencido" id="nominal-anual-ano-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Nominal Anual Año Anticipado</label></td>
+	        <td><input type="text" name="nominal-anual-ano-anticipado" id="nominal-anual-ano-anticipado" required class="form-control"></td>
+	      </tr>
+
+	      
+	      
+	    </tbody>
+	  </table>
+
+	</div>
+	<div class="col-md-6">
+		
+		<h3>Periodicas</h3>
+
+		<table class="table table-bordered">
+	    <thead>
+	      <tr>
+	      
+	      </tr>
+	    </thead>
+	    <tbody>
+	      <tr>
+	        <td><label>Diario</label></td>
+	        <td><input type="text" name="periodica-diario" id="periodica-diario" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Mensual Vencido</label></td>
+	        <td><input type="text" name="periodica-mensual-vencido" id="periodica-mensual-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Mensual Anticipado</label></td>
+	        <td><input type="text" name="periodica-mensual-anticipado" id="periodica-mensual-anticipado" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Bimestral Vencido</label></td>
+	        <td><input type="text" name="periodica-bimestral-vencido" id="periodica-bimestral-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Bimestral Anticipado</label></td>
+	        <td><input type="text" name="periodica-bimestral-anticipado" id="periodica-bimestral-anticipado" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Trimestral Vencido</label></td>
+	        <td><input type="text" name="periodica-trimestral-vencido" id="periodica-trimestral-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Trimestral Anticipado</label></td>
+	        <td><input type="text" name="periodica-trimestral-anticipado" id="periodica-trimestral-anticipado" required class="form-control"></td>
+	      </tr>
+	       <tr>
+	        <td><label>Semestral Vencido</label></td>
+	        <td><input type="text" name="periodica-semestral-vencido" id="periodica-semestral-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Semestral Anticipado</label></td>
+	        <td><input type="text" name="periodica-semestral-anticipado" id="periodica-semestral-anticipado" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Anual Vencido</label></td>
+	        <td><input type="text" name="periodica-anual-vencido" id="periodica-anual-vencido" required class="form-control"></td>
+	      </tr>
+	      <tr>
+	        <td><label>Anual Anticipado</label></td>
+	        <td><input type="text" name="periodica-anual-anticipado" id="periodica-anual-anticipado" required class="form-control"></td>
+	      </tr>
+
+	     
+
+	      
+	      
+	    </tbody>
+	  </table>
+	</div>
+</div>
